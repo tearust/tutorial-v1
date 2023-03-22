d96c54915f52ad19995de8aa9f7b5ac5970f1d54
Yan Mingzhi <realraindust@gmail.com>
2023-03-22 10:54:40 +0800

    improve cross move related

--- sample-txn-executor/impl/src/account.rs
+++ sample-txn-executor/impl/src/account.rs
@@ -1,13 +1,10 @@
-use crate::{error::Result, sql::sum_task_deposit};
+use crate::{error::Result, sql::sum_task_deposit, utils::my_token_id};
 use sample_txn_executor_codec::txn::Task;
 use tea_sdk::{
     actor_txns::{context::TokenContext, Tsid},
     serialize,
-    tapp::{Account, Balance, PUBLIC_RESERVED_ACCOUNT},
-    utils::wasm_actor::actors::{
-        env::tappstore_id,
-        tokenstate::{cross_move, mov},
-    },
+    tapp::{Account, Balance, TokenId, PUBLIC_RESERVED_ACCOUNT},
+    utils::wasm_actor::actors::{env::tappstore_id, tokenstate::cross_move},
     OptionExt, ResultExt,
 };
 
@@ -18,7 +15,7 @@ pub(crate) async fn deposit_for_task(
     amount: Balance,
     ctx: Vec<u8>,
 ) -> Result<(Vec<u8>, Vec<u8>)> {
-    let tappstore_ctx = tappstore_ctx(tsid, base).await?;
+    let tappstore_ctx = tappstore_ctx(tsid, base, Some(my_token_id())).await?;
     cross_move(from, PUBLIC_RESERVED_ACCOUNT, amount, tappstore_ctx, ctx)
         .await
         .err_into()
@@ -30,7 +27,7 @@ pub(crate) async fn rollback_deposit(
     task: &Task,
     ctx: Vec<u8>,
 ) -> Result<(Vec<u8>, Vec<u8>)> {
-    let tappstore_ctx = tappstore_ctx(tsid, base).await?;
+    let tappstore_ctx = tappstore_ctx(tsid, base, None).await?;
     cross_move(
         PUBLIC_RESERVED_ACCOUNT,
         task.creator,
@@ -46,17 +43,36 @@ pub(crate) async fn reward_owner(
     tsid: Tsid,
     base: Tsid,
     task: &Task,
-    mut ctx: Vec<u8>,
+    ctx: Vec<u8>,
 ) -> Result<(Vec<u8>, Vec<u8>)> {
     let deposit_sum = sum_task_deposit(&task.subject).await?;
-    let tappstore_ctx = tappstore_ctx(tsid, base).await?;
+    let tappstore_ctx = tappstore_ctx(tsid, base, None).await?;
     let worker = task.worker.ok_or_err("worker")?;
-    ctx = mov(PUBLIC_RESERVED_ACCOUNT, worker, deposit_sum, ctx).await?;
-    cross_move(worker, worker, deposit_sum, ctx, tappstore_ctx)
-        .await
-        .err_into()
+    cross_move(
+        PUBLIC_RESERVED_ACCOUNT,
+        worker,
+        deposit_sum,
+        ctx,
+        tappstore_ctx,
+    )
+    .await
+    .err_into()
 }
 
-pub(crate) async fn tappstore_ctx(tsid: Tsid, base: Tsid) -> Result<Vec<u8>> {
-    serialize(&TokenContext::new_slim(tsid, base, tappstore_id().await?)).err_into()
+pub(crate) async fn tappstore_ctx(
+    tsid: Tsid,
+    base: Tsid,
+    allowance_tid: Option<TokenId>,
+) -> Result<Vec<u8>> {
+    if let Some(token_id) = allowance_tid {
+        serialize(&TokenContext::new_cross_move(
+            tsid,
+            base,
+            tappstore_id().await?,
+            token_id,
+        ))
+        .err_into()
+    } else {
+        serialize(&TokenContext::new_slim(tsid, base, tappstore_id().await?)).err_into()
+    }
 }
